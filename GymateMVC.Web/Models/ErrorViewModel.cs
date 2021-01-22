f4f2590bef40a5948cfef3b8e3348003ed9c8e7d
Glina1993 <jakub.glinski93@gmail.com>
2021-01-22 21:54:46 +0100

    Add project files.

---
+++ GymateMVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GymateMVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
