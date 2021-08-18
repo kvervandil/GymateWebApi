e187186738b59cfb8bfb2f345ceabb3a6dc1b480
Jakub Gliński <jglinski@pgs-soft.com>
2021-08-18 19:03:42 +0200

    update sql connection

--- GymateWebApi.Api/Startup.cs
+++ GymateWebApi.Api/Startup.cs
@@ -2,6 +2,7 @@ using GymateMVC.Application;
 using GymateMVC.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -49,6 +50,11 @@ namespace GymateWebApi.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
